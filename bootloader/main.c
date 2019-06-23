4a16169a4e204811e9791d3faa33aa5da3623122
Jacob Lewallen <jlewallen@gmail.com>
2019-06-22 19:40:20 -0700

    Scan for firmware.
    This confirms the PIC of a main binary is working.

--- bootloader/main.c
+++ bootloader/main.c
@@ -148,33 +148,42 @@ uint32_t analyse_table(fkb_header_t *header) {
 }
 
 uint32_t fkb_check_find(void *ptr, fkb_found_t *fkbf) {
-    fkb_header_t *fkbh = (fkb_header_t *)ptr;
+    fkb_header_t *selected = NULL;
 
-    fkbf->ptr = NULL;
+    while (1) {
+        fkb_header_t *fkbh = (fkb_header_t *)ptr;
+        debug_println("bl: [0x%08p] checking for header", ptr);
 
-    debug_println("bl: [0x%08p] checking for header", ptr);
+        if (strcmp(fkbh->signature, "FKB") != 0) {
+            break;
+        }
 
-    if (strcmp(fkbh->signature, "FKB") != 0) {
+        selected = fkbh;
+
+        debug_println("bl: [0x%08p] found ('%s') flags=0x%x size=%lu vtor=0x%x", ptr,
+                      fkbh->firmware.name, fkbh->firmware.flags, fkbh->firmware.binary_size,
+                      fkbh->firmware.vtor_offset);
+
+        char hex_hash[(fkbh->firmware.hash_size * 2) + 1];
+        bytes_to_hex(hex_hash, sizeof(hex_hash), fkbh->firmware.hash, fkbh->firmware.hash_size);
+
+        debug_println("bl: [0x%08p] hash='%s' timestamp=%lu", ptr,
+                      hex_hash, fkbh->firmware.timestamp);
+
+        analyse_table(fkbh);
+
+        ptr += fkbh->firmware.binary_size;
+    }
+
+    if (selected == NULL) {
         return 0;
     }
 
-    debug_println("bl: [0x%08p] found ('%s') flags=0x%x size=%lu vtor=0x%x", ptr,
-                  fkbh->firmware.name, fkbh->firmware.flags, fkbh->firmware.binary_size,
-                  fkbh->firmware.vtor_offset);
-
-    char hex_hash[(fkbh->firmware.hash_size * 2) + 1];
-    bytes_to_hex(hex_hash, sizeof(hex_hash), fkbh->firmware.hash, fkbh->firmware.hash_size);
-
-    debug_println("bl: [0x%08p] hash='%s' timestamp=%lu", ptr,
-                  hex_hash, fkbh->firmware.timestamp);
-
-    analyse_table(fkbh);
-
     /* This will need some future customization. I'm considering also placing
      * the header after the vector table, which is more efficient. */
-    fkbf->ptr = (void *)((uint8_t *)ptr) + fkbh->firmware.vtor_offset;
+    fkbf->ptr = (void *)((uint8_t *)selected + selected->firmware.vtor_offset);
 
-    return try_launch((uint32_t *)fkbf->ptr, 0x20000000 + fkbh->firmware.got_offset);
+    return try_launch((uint32_t *)fkbf->ptr, 0x20000000 + selected->firmware.got_offset);
 }
 
 uint32_t launch() {
