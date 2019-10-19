f543c667332ee447a44671e250306f55933d5f17
Jacob Lewallen <jlewallen@gmail.com>
2019-10-19 10:54:58 -0700

    Push got value into stack so the app has a way to verify.

--- src/arch/m4.s
+++ src/arch/m4.s
@@ -8,6 +8,9 @@ invoke_pic:
 
         msr     MSP, r0
         mov     r9, r2
+
+        /* Push GOT value onto stack, the application can then use this to check for corruption. */
+        push    {r9}
         bx      r1
 
         .fnend
